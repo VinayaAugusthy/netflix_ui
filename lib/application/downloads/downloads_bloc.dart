84843dd2108f0893933d3a8870f81bf164b7d566
VinayaAugusthy <vinayaaugusthy@gmail.com>
2024-01-22 18:05:27 +0530

    bloc

---
+++ lib/application/downloads/downloads_bloc.dart
@@ -0,0 +1,38 @@
+import 'package:bloc/bloc.dart';
+
+import 'package:dartz/dartz.dart';
+
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'package:netflix/domain/core/failures/main_failure.dart';
+
+
+import 'package:netflix/domain/model/downloads/download_model.dart';
+
+
+part 'downloads_event.dart';
+
+
+part 'downloads_state.dart';
+
+part 'downloads_bloc.freezed.dart';
+
+
+class DownloadsBloc extends Bloc<DownloadsEvent, DownloadsState> {
+
+  DownloadsBloc() : super(DownloadsState.initial()) {
+
+    on<_GetDownloadsImages>((event, emit) {
+
+      emit(state.copyWith(
+        isLoading: true,
+        downloadsFailureOrSuccessOption: none(),
+      ));
+
+    });
+
+  }
+
+}
+
