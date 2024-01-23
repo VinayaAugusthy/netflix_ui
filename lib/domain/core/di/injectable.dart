9c8562ddc89ab6392d2964eaa8dde48105fb49ae
VinayaAugusthy <vinayaaugusthy@gmail.com>
2024-01-23 10:59:52 +0530

    injection done

--- lib/domain/core/di/injectable.dart
+++ lib/domain/core/di/injectable.dart
@@ -1,6 +1,9 @@
+import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:netflix/domain/core/di/injectable.config.dart';
 
-@injectableInit
-Future<void> configureInjection() async{
-await 
+final getIt = GetIt.instance;
+@InjectableInit()
+Future<void> configureInjection() async {
+  await $initGetIt(getIt,environment: Environment.prod);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:netflix/core/colors/colors.dart';
+import 'package:netflix/domain/core/di/injectable.dart';
 import 'package:netflix/presentation/main_page/widgets/screen_main_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await configureInjection();
   runApp(const MyApp());
-  
 }
 
 class MyApp extends StatelessWidget {
@@ -19,15 +21,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.black,
         appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent),
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue).copyWith(background: backgroundColorApp),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
+            .copyWith(background: backgroundColorApp),
         fontFamily: GoogleFonts.montserrat().fontFamily,
         textTheme: const TextTheme(
-          bodyLarge: TextStyle(
-            color: Colors.white
-          ),
-          bodyMedium: TextStyle(
-            color: Colors.white
-          ),
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
         ),
       ),
       home: const MainScreen(),
